04b2a036529f0dbdea64b88438a18f934c54267b
Ian Garrett <igarrett@uoregon.edu>
2015-11-30 22:08:52 -0800

    working project 7

--- main.py
+++ main.py
@@ -2,23 +2,22 @@ import flask
 from flask import render_template
 from flask import request
 from flask import url_for
-import uuid
 
 import json
 import logging
+import uuid
 
-# Date handling 
+# Imports to handle dates
 import arrow # Replacement for datetime, based on moment.js
 import datetime # But we still need time
 from dateutil import tz  # For interpreting local times
 
+# Google API for services
+from apiclient import discovery
 
 # OAuth2  - Google library implementation for convenience
 from oauth2client import client
 import httplib2   # used in oauth2 flow
-
-# Google API for services 
-from apiclient import discovery
 
 ###
 # Globals
@@ -28,7 +27,7 @@ app = flask.Flask(__name__)
 
 SCOPES = 'https://www.googleapis.com/auth/calendar.readonly'
 CLIENT_SECRET_FILE = CONFIG.GOOGLE_LICENSE_KEY  ## You'll need this
-APPLICATION_NAME = 'MeetMe class project'
+APPLICATION_NAME = 'MeetMe'
 
 #############################
 #
@@ -46,16 +45,13 @@ def index():
 
 @app.route("/choose")
 def choose():
-    ## We'll need authorization to list calendars 
-    ## I wanted to put what follows into a function, but had
-    ## to pull it back here because the redirect has to be a
-    ## 'return' 
-    app.logger.debug("Checking credentials for Google calendar access")
+    # Authorize list
+    app.logger.debug("Checking credentials for Gooasgle calendar access")
     credentials = valid_credentials()
     if not credentials:
       app.logger.debug("Redirecting to authorization")
       return flask.redirect(flask.url_for('oauth2callback'))
-
+    global gcal_service #used in busyTimes
     gcal_service = get_gcal_service(credentials)
     app.logger.debug("Returned from get_gcal_service")
     flask.session['calendars'] = list_calendars(gcal_service)
@@ -69,7 +65,7 @@ def choose():
 #      redirect to OAuth server first, and may take multiple
 #      trips through the oauth2 callback function.
 #
-#  Protocol for use ON EACH REQUEST: 
+#  Protocol for use ON EACH REQUEST:
 #     First, check for valid credentials
 #     If we don't have valid credentials
 #         Get credentials (jump to the oauth2 protocol)
@@ -82,11 +78,10 @@ def choose():
 #  from the Google services. Service objects are NOT serializable ---
 #  we can't stash one in a cookie.  Instead, on each request we
 #  get a fresh serivce object from our credentials, which are
-#  serializable. 
+#  serializable.
 #
 #  Note that after authorization we always redirect to /choose;
 #  If this is unsatisfactory, we'll need a session variable to use
-#  as a 'continuation' or 'return address' to use instead. 
 #
 ####
 
@@ -94,8 +89,8 @@ def valid_credentials():
     """
     Returns OAuth2 credentials if we have valid
     credentials in the session.  This is a 'truthy' value.
-    Return None if we don't have credentials, or if they
-    have expired or are otherwise invalid.  This is a 'falsy' value. 
+    Return None if we don't have credentials, if not, return 
+    a 'falsy' value.
     """
     if 'credentials' not in flask.session:
       return None
@@ -139,13 +134,10 @@ def oauth2callback():
       CLIENT_SECRET_FILE,
       scope= SCOPES,
       redirect_uri=flask.url_for('oauth2callback', _external=True))
-  ## Note we are *not* redirecting above.  We are noting *where*
-  ## we will redirect to, which is this function. 
-  
-  ## The *second* time we enter here, it's a callback 
+
+  ## The *second* time we enter here, it's a callback
   ## with 'code' set in the URL parameter.  If we don't
   ## see that, it must be the first time through, so we
-  ## need to do step 1. 
   app.logger.debug("Got flow")
   if 'code' not in flask.request.args:
     app.logger.debug("Code not in flask.request.args")
@@ -173,7 +165,7 @@ def oauth2callback():
 #     computation here; use of the information might
 #     depend on what other information we have.
 #   Setting an option sends us back to the main display
-#      page, where we may put the new information to use. 
+#      page
 #
 #####
 
@@ -183,29 +175,37 @@ def setrange():
     User chose a date range with the bootstrap daterange
     widget.
     """
-    app.logger.debug("Entering setrange")  
-    flask.flash("Setrange gave us '{}'".format(
-      request.form.get('daterange')))
     daterange = request.form.get('daterange')
     flask.session['daterange'] = daterange
     daterange_parts = daterange.split()
     flask.session['begin_date'] = interpret_date(daterange_parts[0])
     flask.session['end_date'] = interpret_date(daterange_parts[2])
     app.logger.debug("Setrange parsed {} - {}  dates as {} - {}".format(
-      daterange_parts[0], daterange_parts[1], 
+      daterange_parts[0], daterange_parts[1],
       flask.session['begin_date'], flask.session['end_date']))
-    return flask.redirect(flask.url_for("choose"))
+    returnRange = flask.redirect(flask.url_for("choose"))
+    return returnRange
+
+@app.route('/select_calendars', methods=['POST'])
+def getCalendars():
+    app.logger.debug("Get selected caldendars")
+    selectedCalendars = request.form.getlist('calendar')
+    allCalendars = []
+    for cal in flask.session['calendars']:
+        if cal['id'] in selectedCalendars:
+            allCalendars.append(cal)
+    generateFreeTimes(allCalendars)
+    return flask.redirect(flask.url_for("index"))
 
 ####
 #
-#   Initialize session variables 
+#   Initialize session variables
 #
 ####
 
 def init_session_values():
     """
     Start with some reasonable defaults for date and time ranges.
-    Note this must be run in app context ... can't call from main. 
     """
     # Default date span = tomorrow to 1 week from now
     now = arrow.now('local')
@@ -229,7 +229,7 @@ def interpret_time( text ):
     """
     app.logger.debug("Decoding time '{}'".format(text))
     time_formats = ["ha", "h:mma",  "h:mm a", "H:mm"]
-    try: 
+    try:
         as_arrow = arrow.get(text, time_formats).replace(tzinfo=tz.tzlocal())
         app.logger.debug("Succeeded interpreting time")
     except:
@@ -264,7 +264,107 @@ def next_day(isotext):
 #  Functions (NOT pages) that return some information
 #
 ####
-  
+
+
+def calendarEventsList(calendarList):
+    # gets events from selected calendars
+    busyTimes = []
+    beginDate = flask.session['begin_date']
+    endDate = flask.session['end_date']
+    endDate = arrow.get(endDate).replace(hours=+24).isoformat() # 24 hours added is to split into days
+    for calendar in calendarList:
+        ID = calendar['id']
+        freebusy_query = {
+            "timeMin" : beginDate,
+            "timeMax" : endDate,
+            "items" : [{ "id" : ID }]
+        }
+        result = gcal_service.freebusy().query(body=freebusy_query).execute()
+        resultTimes = result['calendars'][ID]['busy']
+        busyTimes.append(resultTimes)
+    return busyTimes
+
+def sortEvents(eventList):
+    # sort list by start time and return sorted list
+    startTimes = []
+    sortedTimes = []
+    for event in eventList: # loop through events and add all start times to startTimes
+        startTimes.append(event['start'])
+    startTimes.sort() # sort by start time
+    for times in startTimes: # loop through start times and create events to append to sortedTimes
+        for event in eventList:
+            if (times == event['start']):
+                sortedTimes.append({'start':event['start'], 'end':event['end']})
+    return sortedTimes
+
+def addNights(eventList):
+    # add busy events every day from 7am-9pm
+    startDate = arrow.get(flask.session['begin_date'])
+    endDate = arrow.get(flask.session['end_date'])
+    for day in arrow.Arrow.span_range('day', startDate, endDate): # loop through days in range
+        startOfDay= {'start':day[0], 'end':day[0].replace(hours=+7)}
+        endOfDay = {'start':day[1].replace(hours=-3).replace(seconds=+.000001), 'end':day[1].replace(seconds=+.000001)}
+        eventList.append(startOfDay)
+        eventList.append(endOfDay)
+    return eventList
+
+def generateFreeTimes(calendarList):
+    #put event times into a collective list
+    allLists = []
+    for cal in calendarEventsList(calendarList):
+        if cal: #In case list is empty
+            for event in cal:
+                start = arrow.get(event['start']).to('local')
+                end = arrow.get(event['end']).to('local')
+                allLists.append({'start':start, 'end':end})
+
+    allLists = addNights(allLists) #add all times between 9pm-7am as a busy event
+    sortedEvents = sortEvents(allLists) #sort the events
+    freeTimes = fetchFreeTimes(sortedEvents) #fetch the list of free times
+
+    displayTimes(freeTimes)
+
+def displayTimes(freeTimes):
+    #format time-ranges in a easy-to-read, easy-to-analyze format
+    for times in freeTimes:
+        message = []
+        message.append("From ")
+        message.append(times['start'].format('MM/DD/YYYY h:mm A'))
+        message.append(" until ")
+        message.append(times['end'].format('MM/DD/YYYY h:mm A'))
+        message = ''.join(message)
+        flask.flash(message)
+
+def fetchFreeTimes(sortedList):
+    correctedSortedList = duplicateRemover(sortedList) # call to remove duplicates
+
+    freeTimes = []
+    length = len(correctedSortedList)-1
+    for i in range(length): # loop through events, and create free times out of gaps between busy events
+        event = correctedSortedList[i]
+        nextEvent = correctedSortedList[i+1]
+        if nextEvent['start'] > event['end']: # if the start of the next event occurs after the end of the current event
+            freeTimes.append({'start':event['end'], 'end':nextEvent['start']}) # append the time between to free times
+    return freeTimes
+
+def duplicateRemover(eventList): 
+    #corrects overlaps in busy times
+    correctedList = []
+    for i in range(len(eventList)-1):
+        event = eventList[i]
+        nextEvent = eventList[i+1]
+        event['end'].replace
+        if (event['end'] > nextEvent['end'] and nextEvent['start'] < event['end']):
+            correctedList.append({'start':event['start'], 'end':event['end']})
+            eventList[i+1]['end'] = event['end'] # correctly iterates
+        elif (event['end'] > nextEvent['start']):
+            correctedList.append({'start':event['start'], 'end':nextEvent['start']})
+        else:
+            correctedList.append({'start':event['start'], 'end':event['end']})
+    correctedList.append(eventList[len(eventList)-1]) # event is added to correctedList
+    return correctedList
+
+
 def list_calendars(service):
     """
     Given a google 'service' object, return a list of
@@ -274,13 +374,13 @@ def list_calendars(service):
     the primary calendar first, and selected (that is, displayed in
     Google Calendars web app) calendars before unselected calendars.
     """
-    app.logger.debug("Entering list_calendars")  
+    app.logger.debug("Entering list_calendars")
     calendar_list = service.calendarList().list().execute()["items"]
     result = [ ]
     for cal in calendar_list:
         kind = cal["kind"]
         id = cal["id"]
-        if "description" in cal: 
+        if "description" in cal:
             desc = cal["description"]
         else:
             desc = "(no description)"
@@ -288,7 +388,7 @@ def list_calendars(service):
         # Optional binary attributes with False as default
         selected = ("selected" in cal) and cal["selected"]
         primary = ("primary" in cal) and cal["primary"]
-        
+
 
         result.append(
           { "kind": kind,
@@ -325,7 +425,7 @@ def cal_sort_key( cal ):
 
 @app.template_filter( 'fmtdate' )
 def format_arrow_date( date ):
-    try: 
+    try:
         normal = arrow.get( date )
         return normal.format("ddd MM/DD/YYYY")
     except:
@@ -338,7 +438,7 @@ def format_arrow_time( time ):
         return normal.format("HH:mm")
     except:
         return "(bad time)"
-    
+
 #############
 
 
@@ -347,7 +447,7 @@ if __name__ == "__main__":
   # exist whether this is 'main' or not
   # (e.g., if we are running in a CGI script)
 
-  app.secret_key = str(uuid.uuid4())  
+  app.secret_key = str(uuid.uuid4())
   app.debug=CONFIG.DEBUG
   app.logger.setLevel(logging.DEBUG)
   # We run on localhost only if debugging,
@@ -356,6 +456,6 @@ if __name__ == "__main__":
     # Reachable only from the same computer
     app.run(port=CONFIG.PORT)
   else:
-    # Reachable from anywhere 
+    # Reachable from anywhere
     app.run(port=CONFIG.PORT,host="0.0.0.0")
-    
+
